193d66cd52da00ee8c6f7ed609feed55466578d6
Binamra Neupane <nbinamra@gmail.com>
2025-09-08 19:15:07 -0400

    first commit

---
+++ src/main/java/com/lcwd/user/service/Hotel_Rating/HotelRatingApplication.java
@@ -0,0 +1,13 @@
+package com.lcwd.user.service.Hotel_Rating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelRatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelRatingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lcwd/user/service/Hotel_Rating/HotelRatingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcwd.user.service.Hotel_Rating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelRatingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
